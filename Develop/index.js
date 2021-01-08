1deafffba5ef93247e4dbd5107eb70e68a691758
Drew <bridgmand@gmail.com>
2021-01-08 11:32:02 -0500

    added packages, prompt questions, and init function

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+const inquirer = require ('inquirer');
+const fs = require ('fs');
+const util = require ('util');
+const generateMarkdown = require('./utils/generateMarkdown');
+const writeFileAsync = util.promisify(fs.writeFile)
 
 // TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const promptUser = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What do you want to name the application?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a description of the application.',
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What is the installation process of this application?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the usage for this application',
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'What license if any does this application hold?',
+        },
+        {
+            type: 'input',
+            name: 'contribute',
+            message: 'How can someone contribute to this application?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'How does someone test this application?',
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'How can I submit any questions I have about this application?',
+        }
+
+    ]);
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = async () => {
+    try {
+        const answers = await promptUser();
+        const html = generateMarkdown(answers);
+        await writeFileAsync('index.html', html)
+        console.log('successful')
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
 
 // Function call to initialize app
 init();
